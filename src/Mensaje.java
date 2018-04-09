82eb801e2a52dde4a3dd50f20c6eb11a9fa95bcc
MarcoJL066 <38052746+MarcoJL066@users.noreply.github.com>
2018-04-09 17:06:42 -0500

    Update Mensaje.java

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("nuevo intento 2");
+        System.out.println("Esto esta cerca");
 	}
 
 }
