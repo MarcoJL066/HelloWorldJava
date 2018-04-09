6941adef64656930b7b6dd41d925c97eb0e2d52f
MarcoJL066 <38052746+MarcoJL066@users.noreply.github.com>
2018-04-09 10:01:22 -0500

    Update Mensaje.java

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Nooooo casi :¨C");
+        System.out.println("Nooooo casi, porque? :¨C");
 	}
 
 }
