3126cbbb13df99ed88c4b62938174973b425e816
MarcoJL066 <38052746+MarcoJL066@users.noreply.github.com>
2018-04-09 12:20:39 -0500

    Update Mensaje.java

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Casi");
+        System.out.println("ya valió");
 	}
 
 }
